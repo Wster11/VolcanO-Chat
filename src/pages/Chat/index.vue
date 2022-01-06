76c96a2f15ce890758a1eda7e1498001270b2fad
stwang <464905943@qq.com>
2022-01-06 19:55:05 +0800

    feat: 表情添加完成

--- src/pages/Chat/index.vue
+++ src/pages/Chat/index.vue
@@ -12,7 +12,26 @@
     </div>
     <div class="sendMsgWrap">
       <div class="richMsgWrap">
-        <Icon class="icon" size="20" name="smile-o" />
+        <Popover v-model:show="chat.emojiShow" placement="top-start">
+          <Grid
+            square
+            clickable
+            :border="false"
+            column-num="10"
+            style="width: 240px; paddingbottom: 5px"
+          >
+            <GridItem
+              v-for="i in chat.emojiLs"
+              :key="i"
+              @click="showPopover = false"
+            >
+              <img class="emoji" :src="i" @click="chat.selectEmoji" />
+            </GridItem>
+          </Grid>
+          <template #reference>
+            <Icon class="icon" size="20" name="smile-o" />
+          </template>
+        </Popover>
         <Icon class="icon" size="20" name="photo-o" />
       </div>
       <Input ref="ipt" @send="chat.sendMsg" />
@@ -24,14 +43,15 @@
 import { Options, Vue, setup } from "vue-class-component";
 import { useRouter, Router, useRoute } from "vue-router";
 import Input from "@/components/input.vue";
-import { NavBar, Icon } from "vant";
+import { NavBar, Icon, Popover, Grid, GridItem } from "vant";
 import { useStore } from "vuex";
-import { getCurrentInstance } from "vue";
+import { getCurrentInstance, ref } from "vue";
 import websdk, { EasemobChat } from "easemob-websdk";
 import { MSG_TYPE, CHAT_TYPE } from "@/const";
 import { ERROR_CODE } from "@/const/errorCode";
 import MsgLeft from "@/components/messageLeft.vue";
 import MsgRight from "@/components/messageRight.vue";
+import emoji from "@/const/emojs";
 
 @Options({
   components: {
@@ -39,7 +59,10 @@ import MsgRight from "@/components/messageRight.vue";
     Input,
     Icon,
     MsgLeft,
-    MsgRight
+    MsgRight,
+    Popover,
+    Grid,
+    GridItem
   }
 })
 export default class Contact extends Vue {
@@ -50,6 +73,16 @@ export default class Contact extends Vue {
     const conn = store.state.IM.connect;
     const instance = getCurrentInstance();
     const fromId = route.params.fromId as string;
+    const emojiShow = ref(false);
+
+    const emojiLs = Object.values(emoji.obj).map((item) => {
+      return require(`../../static/emoji/${item}`);
+    });
+
+    const selectEmoji = () => {
+      emojiShow.value = false;
+    };
+
     const toChat = () => {
       router.push("/chat");
     };
@@ -83,6 +116,9 @@ export default class Contact extends Vue {
       toChat,
       sendMsg,
       fromId: fromId,
+      emojiShow,
+      selectEmoji: selectEmoji,
+      emojiLs: emojiLs,
       chatInfo: store.state.IM.chat
     };
   });
@@ -108,5 +144,10 @@ export default class Contact extends Vue {
 }
 .icon {
   margin-right: 2vw;
+}
+
+.emoji {
+  width: 20px;
+  height: 20px;
 }
 </style>
