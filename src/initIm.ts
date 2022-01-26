140506bb661557ad784e710caeb7add19f8ae4a0
stwang <464905943@qq.com>
2022-01-26 14:27:41 +0800

    delete im config

--- src/initIm.ts
+++ src/initIm.ts
@@ -4,9 +4,6 @@ import { appKey } from "@/const/index";
 const conn = new websdk.connection({
   appKey,
   delivery: true,
-  url: `${window.location.protocol}//im-api-v2-hsb.easemob.com/ws`,
-  apiUrl: `${window.location.protocol}//a1-hsb.easemob.com`,
-  isHttpDNS: false
   // useOwnUploadFun: true
 });
 
